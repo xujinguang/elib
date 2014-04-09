7aab8358a2e814f1f1a6d198b3b3a0bd0484eda7
xujinguang <zqbxsx@126.com>
2014-04-10 00:20:43 +0800

    container_of

---
+++ basic.h
@@ -0,0 +1,8 @@
+#ifndef _BASIC_H_
+#define _BASIC_H_
+
+#define container_of(ptr, type, member) ({                      \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+    (type *)( (char *)__mptr - offsetof(type,member) );})
+
+#endif
